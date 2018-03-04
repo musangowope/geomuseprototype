4984dc46ffdb22895499d01d04a1f085baab24c6
Musango <Musango@bitbucket.org>
2018-03-04 11:54:58 +0200

    master build

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'geomuse.eu.auth0.com',
   clientId: 'B0n4Z5GqfEG6hEOOTZPrLE8rOkvGTpbp',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://geomuse-388cd.firebaseapp.com/callback'
 }
